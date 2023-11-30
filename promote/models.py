b4c70f9650a9cf7faeecd56a310f21ef50ebe2ba
hytionyun <soiyosauce@gmail.com>
2023-12-01 00:35:07 +0900

    4

--- promote/models.py
+++ promote/models.py
@@ -1,7 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class MainContent(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField('date_published')
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content_list = models.ForeignKey(MainContent, on_delete=models.CASCADE)
+    #자신과 다대일 관계만들려면 : models.ForeignKey('self', on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True) # 최초 시간 저장
+    modify_date = models.DateTimeField(auto_now=True) # 댓글 수정시 수정 시간 정함
--- promote/urls.py
+++ promote/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index), # /promote 로 promote/views.py의 index() 연결
     path('<int:content_id>/', views.detail, name='detail'), # /promote/content_id 로 views.py의 detail() 연결, name을 작성하여 .html url 연동에서 하드코딩 방지
+    path('comment/create/<int:content_id>/', views.comment_create, name='comment_create'),
+    path('comment/update/<int:comment_id>/', views.comment_update,name='comment_update'),
+    path('comment/delete/<int:comment_id>/', views.comment_delete, name='comment_delete'),
 ]
